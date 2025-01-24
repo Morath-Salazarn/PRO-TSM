37b861c0a6a33aa4721d56b57d3d6810cd410344
Morath-Salazarn <maximilian.leidl@gmx.net>
2025-01-24 10:37:25 +0100

    pro directories add

---
+++ C++/zusatzaufgabe vector.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main ()
+{
+    vector<int> E6Norm;
+    E6Norm.push_back(100);
+    E6Norm.push_back(150);
+    E6Norm.push_back(220);
+    E6Norm.push_back(330);
+    E6Norm.push_back(470);
+    E6Norm.push_back(680);
+    E6Norm.push_back(1000);
+
+    int input = 245;
+    
+    int diff1 = 1000;
+    int diff2 = 1000;
+
+ //   cout << "Eingabe Widerstand zwischen 100 und 1000: ";
+//    cin >> input;
+
+    while (diff2 <= diff1){
+        
+    }
+
+
+
+}
---
+++ Java/01_Auto/src/blogic/Auto.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package blogic;
+
+/**
+ *
+ * @author maxi-leidl
+ */
+public class Auto {
+    private double tankVolumen;
+    private double tankInhalt;
+    private double verbrauchProKm;
+    
+//Konstruktoren    
+    // Standardkonstruktor
+    
+    public Auto(){
+        this.tankInhalt = 20;
+        this.tankVolumen = 60;
+        this.verbrauchProKm = 6.7;
+    }
+
+    public Auto(double tankVolumen, double tankInhalt, double verbrauchProKm) {
+        this.tankVolumen = tankVolumen;
+        this.tankInhalt = tankInhalt;
+        this.verbrauchProKm = verbrauchProKm;
+    }
+
+    public double getTankVolumen() {
+        return tankVolumen;
+    }
+
+    public void setTankVolumen(double tankVolumen) {
+        this.tankVolumen = tankVolumen;
+    }
+
+    public double getTankInhalt() {
+        return tankInhalt;
+    }
+
+    public void setTankInhalt(double tankInhalt) {
+        this.tankInhalt = tankInhalt;
+    }
+
+    public double getVerbrauchProKm() {
+        return verbrauchProKm;
+    }
+
+    public void setVerbrauchProKm(double verbrauchProKm) {
+        this.verbrauchProKm = verbrauchProKm;
+    }
+    
+    
+    
+    
+}
