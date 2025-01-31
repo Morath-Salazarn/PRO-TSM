fbb320c844e25bd9dcf881b0bc51a48b243a7eca
Morath-Salazarn <maximilian.leidl@gmx.net>
2025-01-31 10:15:07 +0100

    moved directories

---
+++ Java/Uebung_Bankkonto/src/uebung_bankkonto/Konto.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uebung_bankkonto;
+
+/**
+ *
+ * @author maxi-leidl
+ */
+public class Konto {
+    
+    private String inhaber;
+    private String kontonummer;
+    private double kontostand;
+
+    
+   //Konstruktor
+    
+    public Konto(String inhaber, String kontonummer) {
+        this.inhaber = inhaber;
+        if(this.kontonummer.length() > 10 || this.kontonummer.length() < 6){
+            this.kontonummer = "**********";
+        }
+        else{
+            this.kontonummer = kontonummer;
+        }
+        this.kontostand = 0.0;
+    }
+    
+    //getter und setter
+
+    public String getInhaber() {
+        return inhaber;
+    }
+
+    public String getKontonummer() {
+        return kontonummer;
+    }
+
+    public double getKontostand() {
+        return kontostand;
+    }
+
+    public void setKontostand(double kontostand) {
+        this.kontostand = kontostand;
+    }
+    
+    
+    
+    
+    public void einzahlen(double betrag){
+        if(betrag > 0){
+            setKontostand(getKontostand() + betrag);
+        }
+    }
+    
+    public boolean auszahlen(double betrag){
+        
+    }
+    
+}
