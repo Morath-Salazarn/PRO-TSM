b0a3243a24242b9452efd63fde64e10b75de155d
Morath-Salazarn <maximilian.leidl@gmx.net>
2025-02-07 08:30:06 +0100

    add

---
+++ Java/Zaehlwerk/src/zaehlwerk/zaehlen.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package zaehlwerk;
+
+/**
+ *
+ * @author maxi-leidl
+ */
+public class zaehlen {
+    
+    public int stand = 0;
+    
+    public void inkrementieren(){
+        
+    };
+    
+    public int getStand(){
+        
+    };
+    
+    public void reset(){
+        
+    };
+    
+    
+}
