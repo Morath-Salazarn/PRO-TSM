da04dcaf3888f0ee8b4ee571379013f9eea28013
Morath-Salazarn <maximilian.leidl@gmx.net>
2025-04-09 14:21:35 +0200

    test

--- Java/Klickzaehler/src/main/java/com/mycompany/klickzaehler/App.java
+++ Java/Klickzaehler/src/main/java/com/mycompany/klickzaehler/App.java
@@ -1,6 +1,7 @@
 package com.mycompany.klickzaehler;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
@@ -8,7 +9,8 @@ import javafx.stage.Stage;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.control.Button;
-
+import javafx.geometry.Insets;
+import javafx.scene.control.TextField;
 
 
 /**
@@ -20,9 +22,17 @@ public class App extends Application {
     public void start(Stage stage) {
         BorderPane root = new BorderPane();
         
+        
         VBox vboxRechts = new VBox();
         
+        Insets pad = new Insets(10, 10, 50, 10);
+        vboxRechts.setPadding(pad);
+        vboxRechts.setSpacing(5);
+                
+        TextField zaehler = new TextField();
+        
         root.setRight(vboxRechts);
+        root.setTop(zaehler);
         
         Button klick = new Button("+");
         Button reset = new Button("reset");
@@ -30,7 +40,7 @@ public class App extends Application {
         vboxRechts.getChildren().add(klick);
         vboxRechts.getChildren().add(reset);
         
-        Scene scene = new Scene(root, 640, 480);
+        Scene scene = new Scene(root, 200, 150);
         stage.setTitle("Klickzähler");
         stage.setScene(scene);
         stage.show();
