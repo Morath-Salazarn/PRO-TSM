0d25ea415c05e70a74dae2070cc3801270f27c17
Morath-Salazarn <maximilian.leidl@gmx.net>
2025-07-04 09:00:40 +0200

    add folder 03

---
+++ Python/TSM/03 - Textdateien und Datenformate auswerten/Logdatei.py
@@ -0,0 +1,4 @@
+with open("/home/maxi-leidl/Documents/PRO Code/PRO-TSM/Python/TSM/03 - Textdateien und Datenformate auswerten/LogVol.dat", "r", encoding="utf-8") as file:
+    for line in file:
+        print(line, end="")
+
